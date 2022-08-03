71d360a66fee5e0d31fdc4e436b1eff737300f5e
Roman Serediuk <box4rom@gmail.com>
2022-08-03 21:57:24 +0300

    task-01 completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryEl.addEventListener('click', onGalleryLinkClick);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>
+</div>`;
+        })
+        .join('');
+}
+
+function onGalleryLinkClick(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+    const largeImgUrl = evt.target.dataset.source;
+    const modalEl = basicLightbox.create(`
+    <img src="${largeImgUrl}" width="800" height="600">
+`);
+
+    modalEl.show();
+
+    window.addEventListener('keydown', onKeyEscPress);
+    function onKeyEscPress(evt) {
+        if (evt.code === 'Escape') {
+            modalEl.close();
+        }
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+    <a class="gallery__item" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+        />
+    </a>
+</div>`;
+        })
+        .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    /* options */
+});
